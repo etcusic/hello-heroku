a942a234159abc937963b259b1c45656698f77df
Eli Cusic <tntitans31@gmail.com>
2021-02-23 11:30:45 -0600

    set up database with user and ingredients

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,5 @@
-# class ApplicationController < ActionController::API
-# end
-class ApplicationController < ActionController::Base
-    protect_from_forgery with: :exception
+class ApplicationController < ActionController::API
 end
+# class ApplicationController < ActionController::Base
+#     protect_from_forgery with: :exception
+# end
